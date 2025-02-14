d254238c41ba0a1a38d9f04dbe7e9cf96c527156
苏志权 <939958149@qq.com>
2025-02-14 16:25:47 +0800

    初始化博客

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  title: '我的博客',
+  description: '这是我的个人博客',
+  themeConfig: {
+    nav: [
+      { text: '主页', link: '/' },
+      { text: '关于', link: '/about/' }
+    ],
+    sidebar: [
+      '/',
+      '/about/'
+    ]
+  }
+}
---
+++ docs/README.md
@@ -0,0 +1,3 @@
+# 欢迎来到我的博客
+
+这是我的个人博客，使用 VuePress 和 GitHub Pages 搭建。
