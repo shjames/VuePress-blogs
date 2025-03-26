b748fb9b2dbe11055550c759a0561ef46f22e4e2
苏志权 <939958149@qq.com>
2025-03-26 14:58:40 +0800

    添加文件

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@
 set -e
 
 # 生成静态文件
-npm run build
+yarn run build
 
 # 进入生成的文件夹
 cd docs/.vuepress/dist
@@ -18,9 +18,9 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+# git push -f git@github.com:shjames/heipi-blogs.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f  https://github.com/shjames/heipi-blogs.git master:gh-pages
 
 cd -
