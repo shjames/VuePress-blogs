e636e66c480ea99aff05813cb15d1e24a5127c8a
苏志权 <939958149@.com>
2025-05-10 19:05:59 +0800

    feat(docs): 添加交通手势图解及相关配置
    在`docs/subjectOne/traffic-gestures/`目录下新增交通手势图解图片和README文件，并在VuePress配置中添加交通手势页面的导航和侧边栏链接。同时，更新了Webpack配置以支持WebP格式图片的加载。

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,12 @@
 module.exports = {
+  chainWebpack: (config, isServer) => {
+    config.module
+      .rule('images')
+      .test(/\.(png|jpe?g|gif|webp)(\?.*)?$/)
+      .use('url-loader')
+      .loader('url-loader')
+      .tap(options => Object.assign(options, { limit: 10240 }));
+  },
   title: "我的博客",
   description: "这是我的个人博客",
   themeConfig: {
@@ -12,6 +20,7 @@ module.exports = {
       { text: "刑事处罚", link: "/subjectOne/traffic-criminal-punishment/" },
       { text: "罚款相关", link: "/subjectOne/traffic-amerce/" },
       { text: "其他分类题目", link: "/subjectOne/traffic-other/" },
+      { text: "交通手势", link: "/subjectOne/traffic-gestures/" },
       { text: "模拟考试错误收集", link: "/subjectOne/mock-exam-errors/" },
     ],
     sidebar: {
@@ -24,6 +33,7 @@ module.exports = {
         "traffic-criminal-punishment/",
         "traffic-amerce/",
         "traffic-other/", // 其他分类题目
+        "traffic-gestures/", // 交通手势
         "mock-exam-errors/", // 模拟考试错误收集
       ],
       "/": [""],
---
+++ docs/subjectOne/traffic-gestures/README.md
@@ -0,0 +1,14 @@
+# 交警手势信号图解
+
+<div class="gesture-container">
+<img src="./images/1.webp" alt="1" width="200"/>
+<img src="./images/2.webp" alt="2" width="200"/>
+<img src="./images/3.webp" alt="3" width="200"/>
+<img src="./images/4.webp" alt="4" width="200"/>
+<img src="./images/5.webp" alt="5" width="200"/>
+<img src="./images/6.webp" alt="6" width="200"/>
+<img src="./images/7.webp" alt="7" width="200"/>
+<img src="./images/8.webp" alt="8" width="200"/>
+
+</div>
+
